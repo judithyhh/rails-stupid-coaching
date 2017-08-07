08a55fcd2a4d0358fae0afa7c83975be94e1f467
Judith Hsiang <judithyhh@gmail.com>
2017-08-07 14:07:51 +0800

    Logic between pages to be completed.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,13 @@
 class QuestionsController < ApplicationController
   def answer
     @question = params[:query]
+    if @question == "I am going to work right now!"
+      return ""
+    elsif @question.include?("?")
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
   end
 
   def ask
