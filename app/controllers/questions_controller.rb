b0243a10af4a83952b3cdf2ade8d1ad922c1e3c7
Judith Hsiang <judithyhh@gmail.com>
2017-08-07 14:48:44 +0800

    Coach answer logic complete. No styling yet.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,11 +2,11 @@ class QuestionsController < ApplicationController
   def answer
     @question = params[:query]
     if @question == "I am going to work right now!"
-      return ""
+      return @answer = ""
     elsif @question.include?("?")
-      return "Silly question, get dressed and go to work!"
+      return @answer = "Silly question, get dressed and go to work!"
     else
-      return "I don't care, get dressed and go to work!"
+      return @answer = "I don't care, get dressed and go to work!"
     end
   end
 
